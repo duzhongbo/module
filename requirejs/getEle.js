7f33d29b6e65d8a6a4193f0d95f3abe3a534751d
duzhongbo <710409599@qq.com>
2016-03-27 14:21:28 +0800

    requirejs模块采用通用模块写法

--- requirejs/getEle.js
+++ requirejs/getEle.js
@@ -1,7 +1,26 @@
-define(function  () {
-	return {
-		$:function (str) {
-			return document.getElementById(str);
-		}
-	}
+// define(function  () {
+// 	return {
+// 		$:function (str) {
+// 			return document.getElementById(str);
+// 		}
+// 	}
+// });
+
+;(function(){
+    function $(str) {
+        return document.getElementById(str);
+    }
+    
+    var moduleName = {
+    	$:$
+    };
+    if (typeof module !== 'undefined' && typeof exports === 'object') {
+        module.exports = moduleName;
+    } else if (typeof define === 'function' && (define.amd || define.cmd)) {
+        define(function() { return moduleName; });
+    } else {
+        this.moduleName = moduleName;
+    }
+}).call(function() {
+    return this || (typeof window !== 'undefined' ? window : global);
 });
