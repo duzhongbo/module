3cdbdfb9d55cc8ebe717a459ce366a7957cac8d4
duzhongbo <710409599@qq.com>
2016-03-27 14:21:54 +0800

    文件目录整理

--- umd/main.js
+++
@@ -1,5 +0,0 @@
-require(['setBgColor'], function (s){
-	J_box.onclick=function () {
-		s.set("J_box","green");
-	}
-});
--- umd/setBgColor.js
+++
@@ -1,8 +0,0 @@
-define(['getEle'],function  (g) {
-	return {
-		set:function (str,color) {
-	        var obj = g.g(str);
-	        obj.style.backgroundColor=color;
-		}
-	}
-});
