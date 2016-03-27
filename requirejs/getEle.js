067dd88c18fed9fe4b392dc9e81e2985c5817abf
duzhongbo <710409599@qq.com>
2016-03-27 15:19:56 +0800

    requirejs模块采用通用模块写法

--- requirejs/getEle.js
+++ requirejs/getEle.js
@@ -6,21 +6,47 @@
 // 	}
 // });
 
-;(function(){
-    function $(str) {
-        return document.getElementById(str);
-    }
+// ;(function(){
+//     function $(str) {
+//         return document.getElementById(str);
+//     }
     
-    var moduleName = {
-    	$:$
-    };
-    if (typeof module !== 'undefined' && typeof exports === 'object') {
-        module.exports = moduleName;
-    } else if (typeof define === 'function' && (define.amd || define.cmd)) {
-        define(function() { return moduleName; });
-    } else {
-        this.moduleName = moduleName;
-    }
-}).call(function() {
-    return this || (typeof window !== 'undefined' ? window : global);
-});
+//     var moduleName = {
+//     	$:$
+//     };
+//     if (typeof module !== 'undefined' && typeof exports === 'object') {
+//         module.exports = moduleName;
+//     } else if (typeof define === 'function' && (define.amd || define.cmd)) {
+//         define(function() { return moduleName; });
+//     } else {
+//         this.moduleName = moduleName;
+//     }
+// }).call(function() {
+//     return this || (typeof window !== 'undefined' ? window : global);
+// });
+
+(
+    function (root, factory) {
+        if (typeof define === 'function' && define.amd) {
+            // AMD
+            define(factory);
+        } else if (typeof exports === 'object') {
+            // Node, CommonJS-like
+            module.exports = factory();
+        } else {
+            // Browser globals (root is window)
+            root.returnExports = factory();
+        }
+    }(this, function () {
+        //    methods
+        function $(str){
+            return document.getElementById(str);
+        };
+
+        //    exposed public method
+        return {
+            $:$
+        };
+    })
+
+);
