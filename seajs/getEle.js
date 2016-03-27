e7bfcdea359392dac75c2b39f4a014bc63f957fd
duzhongbo <710409599@qq.com>
2016-03-27 14:20:13 +0800

    seajs模块采用通用兼容写法

--- seajs/getEle.js
+++ seajs/getEle.js
@@ -1,5 +1,24 @@
-define(function(require, exports) {
-    exports.$=function(str){
+// define(function(require, exports) {
+//     exports.$=function(str){
+//         return document.getElementById(str);
+//     }
+// });
+
+;(function(){
+    function $(str) {
         return document.getElementById(str);
     }
+    
+    var moduleName = {
+    	$:$
+    };
+    if (typeof module !== 'undefined' && typeof exports === 'object') {
+        module.exports = moduleName;
+    } else if (typeof define === 'function' && (define.amd || define.cmd)) {
+        define(function() { return moduleName; });
+    } else {
+        this.moduleName = moduleName;
+    }
+}).call(function() {
+    return this || (typeof window !== 'undefined' ? window : global);
 });
